6e5905bc7cc80d2b9b6d055eb19a5f634fceab48
Matheus Pimentel <matheuspimentel.pimentel@gmail.com>
2022-07-04 22:35:08 -0300

    Criando gráfico com as coordenadas do Kinovea

---
+++ Grafico/Grafico_Chart/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace Grafico_Chart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtArquivos = new System.Windows.Forms.TextBox();
+            this.btnSelecionarArquivo = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnSelecionarArquivo);
+            this.panel1.Controls.Add(this.txtArquivos);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 75);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.chart1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 75);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 375);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Arquivo de coordenadas";
+            // 
+            // txtArquivos
+            // 
+            this.txtArquivos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtArquivos.Location = new System.Drawing.Point(13, 33);
+            this.txtArquivos.Name = "txtArquivos";
+            this.txtArquivos.Size = new System.Drawing.Size(218, 22);
+            this.txtArquivos.TabIndex = 1;
+            // 
+            // btnSelecionarArquivo
+            // 
+            this.btnSelecionarArquivo.Location = new System.Drawing.Point(238, 31);
+            this.btnSelecionarArquivo.Name = "btnSelecionarArquivo";
+            this.btnSelecionarArquivo.Size = new System.Drawing.Size(106, 23);
+            this.btnSelecionarArquivo.TabIndex = 2;
+            this.btnSelecionarArquivo.Text = "Selecionar arquivo";
+            this.btnSelecionarArquivo.UseVisualStyleBackColor = true;
+            this.btnSelecionarArquivo.Click += new System.EventHandler(this.btnSelecionarArquivo_Click);
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(800, 375);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnSelecionarArquivo;
+        private System.Windows.Forms.TextBox txtArquivos;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Grafico/Grafico_Chart/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Grafico_Chart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelecionarArquivo_Click(object sender, EventArgs e)
+        {            
+
+            this.openFileDialog1.Multiselect = false;
+            this.openFileDialog1.Title = "Selecionar .TXT";
+            openFileDialog1.Filter = "Arquivo texto (*.txt)" + "All files (*.*)|*.*";
+
+            openFileDialog1.ShowDialog();
+
+
+            txtArquivos.Text = openFileDialog1.FileName;
+
+            //Exibindo texto completo por linhas
+            string[] lines = File.ReadAllLines(openFileDialog1.FileName);
+
+            Console.WriteLine("O arquivo contem {0} linhas", lines.Length.ToString());
+            int i = 0;
+            char[] delimitadores = new char[] {' ', '.' };
+
+            double[] T, X, Y;
+            int totalLinha = lines.Length - 12;
+
+            T = new double[totalLinha];
+            X = new double[totalLinha];
+            Y = new double[totalLinha];
+
+            int t = 0, x = 0, y = 0, n = 0;
+            int l = 0;
+
+            foreach (string line in lines)
+            {
+                i++;
+
+                if ((!line.Contains("Trajectory")) && i > 8)
+                {
+                  
+                    string[] strings = line.Split(delimitadores);
+                    
+                    foreach (string s in strings){
+                        
+                        if(s != "")
+                        {
+                            if (n == 0)
+                            {
+                                T[t] = Convert.ToDouble(s);                                
+                                t++;
+                                n = 1;                               
+                            }
+                            else if(n == 1)
+                            {
+                                X[x] = Convert.ToDouble(s);                                
+                                x++;
+                                n = 2;                               
+                            }
+                            else
+                            {
+                                Y[y] = Convert.ToDouble(s);
+
+                                y++;
+                                n = 0;                               
+                            }
+                        }
+                        
+                    }                  
+                }                    
+            }
+            /*
+            Console.WriteLine("******************** X ******************");
+            foreach (double line in X)
+            {                
+                Console.WriteLine(line);
+            }
+
+            Console.WriteLine("******************** Y ******************");
+            foreach (double line in Y)
+            {                
+                Console.WriteLine(line);
+            }*/
+
+            this.chart1.Series.Clear();            
+            Series series = this.chart1.Series.Add("Total Income");
+            series.ChartType = SeriesChartType.Spline;
+
+            series.BorderWidth = 1;
+            
+            for(int z = 0; z < X.Length; z++)
+            {
+                series.Points.AddXY(X[z], Y[z]);
+            }
+            
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            this.chart1.Titles.Add("Coordenadas");
+        }
+    }
+}
